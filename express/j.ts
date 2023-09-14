544ae7f68daf5fa6bb82b4cd964ae473a62bbc92
mnsrulz <manishrawat4u@gmail.com>
2023-09-14 23:45:44 +0000

    added a working copy of tesseract

--- express/j.ts
+++ express/j.ts
@@ -4,12 +4,22 @@ import serverless from 'serverless-http';
 const app = express();
 const router = Router();
 import cors from 'cors';
-
+import { recon } from './captcha';
+import { readdirSync } from 'node:fs'
 router.get('/yt', async (req, res) => {
+    const files = walkSync('./', []);
+
     const { v } = req.query;
     if (typeof (v) == 'string') {
+        let resp;
+        try {
+            resp = await recon(v)
+        } catch (error) {
+
+        }
         res.json({
-            hello: 'world'
+            resp,
+            files
         });
     } else {
         res.status(400).json({ error: 'Query param v not defined' });
@@ -21,3 +31,19 @@ app.use(express.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 
 export const handler = serverless(app);
+
+
+var walkSync = function(dir, filelist) {
+    var fs = fs || require('fs'),
+        files = fs.readdirSync(dir);
+    filelist = filelist || [];
+    files.forEach(function(file) {
+      if (fs.statSync(dir + file).isDirectory()) {
+        filelist = walkSync(dir + file + '/', filelist);
+      }
+      else {
+        filelist.push(file);
+      }
+    });
+    return filelist;
+  };
